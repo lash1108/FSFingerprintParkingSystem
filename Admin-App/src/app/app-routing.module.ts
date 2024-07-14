3cc2a8bdf9765e83c8c6037ea913a0090bf6d611
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-07-14 12:27:52 -0600

    sidenav

--- Admin-App/src/app/app-routing.module.ts
+++ Admin-App/src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./components/home/home.component";
+import { HomeComponent } from "./components/home/home.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { SettingsComponent } from "./components/settings/settings.component";
+import {DashboardHomeComponent} from "./components/dashboard-home/dashboard-home.component";
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: 'settings', component: SettingsComponent },
+      { path: 'dashboard-home', component: DashboardHomeComponent },
+      { path: '',redirectTo:'dashboard-home', pathMatch:'full'}
+    ]
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }  // Ruta comodín para redirigir a la ruta por defecto
 ];
 
 @NgModule({
--- Admin-App/src/app/app.module.ts
+++ Admin-App/src/app/app.module.ts
@@ -1,23 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from './components/home/home.component';
 import {MatListModule} from "@angular/material/list";
 import {MatDividerModule} from "@angular/material/divider";
-import {MatCardImage, MatCardModule} from "@angular/material/card";
+import {MatCardModule} from "@angular/material/card";
 import {NgOptimizedImage} from "@angular/common";
-import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {SidenavComponent} from './components/sidenav/sidenav/sidenav.component';
+import {SidenavUtilComponent} from './components/sidenav/sidenav-util/sidenav-util.component';
+import {SidenavBodyComponent} from './components/sidenav/sidenav-body/sidenav-body.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent,
+    SidenavComponent,
+    SidenavUtilComponent,
+    SidenavBodyComponent,
+    SettingsComponent,
+    DashboardHomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,4 +48,5 @@ import {MatInputModule} from "@angular/material/input";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
